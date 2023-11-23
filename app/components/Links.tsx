9ba5b4205e471daef926ecf5adc74ec5435c87d3
Harr-Yates <hyates1@gmail.com>
2023-11-23 11:19:14 +0100

    Update Links component and remove content field in getProjects function

--- app/components/Links.tsx
+++ app/components/Links.tsx
@@ -1,24 +1,32 @@
 export default function Links() {
   return (
-    <div className="flex flex-col items-start w-[205px] text-black-600 underline underline-gray leading-5 pb-14">
+    <div className="flex flex-col items-start w-[205px] text-black-600 leading-5 pb-14 font-medium">
       <a
-        className="hover:underline-bold "
-        href="#">
+        className="underline underline-gray hover:text-bold"
+        href="mailto:hyates1@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer">
         Email
       </a>
       <a
-        className="hover:underline-bold "
-        href="#">
+        className="underline underline-gray hover:text-bold"
+        href="https://read.cv/yates"
+        target="_blank"
+        rel="noopener noreferrer">
         CV
       </a>
       <a
-        className="hover:underline-bold "
-        href="#">
+        className="underline underline-gray hover:text-bold"
+        href="https://github.com/Harry-Yates"
+        target="_blank"
+        rel="noopener noreferrer">
         GitHub
       </a>
       <a
-        className="hover:underline-bold "
-        href="#">
+        className="underline underline-gray hover:text-bold"
+        href="https://www.linkedin.com/in/harryjtyates/"
+        target="_blank"
+        rel="noopener noreferrer">
         LinkedIn
       </a>
     </div>
