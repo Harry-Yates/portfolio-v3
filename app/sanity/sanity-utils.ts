5027daf1b5d3bd07b67e45484c997f8588e92872
Harr-Yates <hyates1@gmail.com>
2023-11-23 20:16:13 +0100

    Fix API version in getProjects function

--- app/sanity/sanity-utils.ts
+++ app/sanity/sanity-utils.ts
@@ -2,25 +2,32 @@ import { Project } from "@/app/types/Project";
 import { createClient, groq } from "next-sanity";
 
 export async function getProjects(): Promise<Project[]> {
-  const client = createClient({
-    projectId: "ofj891ge",
-    dataset: "production",
-    apiVersion: "2021-11-20",
-  });
+  try {
+    const client = createClient({
+      projectId: "ofj891ge",
+      dataset: "production",
+      apiVersion: "2021-11-20",
+    });
 
-  const query = groq`*[_type == "project"] | order(_createdAt desc){
-    _id,
-    _createdAt,
-    name,
-    "slug": slug.current,
-    "video": video.asset->url,
-    url,
-    github,
-    content
-  }`;
+    const query = groq`*[_type == "project"] | order(_createdAt desc){
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "video": video.asset->url,
+      url,
+      github,
+      content
+    }`;
 
-  const projects = await client.fetch(query);
-  console.log(projects);
+    const projects = await client.fetch(query);
+    // const projects = await client.fetch(query, {}, { cache: "no-cache" });
 
-  return projects;
+    console.log(projects);
+
+    return projects;
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    throw error;
+  }
 }
