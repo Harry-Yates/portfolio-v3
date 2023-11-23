078ff77cd490dd5acddd8d857588d2fefabbb915
Harr-Yates <hyates1@gmail.com>
2023-11-23 17:31:17 +0100

    Update page layout in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,8 @@ export const metadata = {
 export default async function Home() {
   const projects = await fetchProjects();
 
-
   return (
-    <div className="flex flex-col min-h-screen mx-auto max-w-5xl px-16 md:px-16 lg:px-8">
+    <div className="flex flex-col min-h-screen mx-auto max-w-5xl px-8 md:px-16 lg:px-8">
       <header className="w-full mx-auto flex flex-col justify-between items-start">
         <h1 className=" font-medium inline mt-20">Harry Yates</h1>
         <p className=" font-light mt-4 mb-8 w-[205px]">
