cc89135c4ff7b08d736c935866145af1899ecd6e
Harr-Yates <hyates1@gmail.com>
2023-11-26 15:31:04 +0100

    Add meta tags for social media sharing images

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,40 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col min-h-screen mx-auto max-w-5xl px-8 md:px-16 lg:px-8">
+      <head>
+        <meta
+          property="og:image"
+          content="https://www.hyates.com/opengraph-image.jpg"
+        />
+        <meta
+          property="og:image:type"
+          content="image/jpeg"
+        />
+        <meta
+          property="og:image:width"
+          content="1200"
+        />
+        <meta
+          property="og:image:height"
+          content="630"
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.hyates.com/twitter-image.jpg"
+        />
+        <meta
+          name="twitter:image:type"
+          content="image/jpeg"
+        />
+        <meta
+          name="twitter:image:width"
+          content="1200"
+        />
+        <meta
+          name="twitter:image:height"
+          content="630"
+        />
+      </head>
       <header className="w-full mx-auto flex flex-col justify-between items-start">
         <h1 className=" font-medium inline mt-20">Harry Yates</h1>
         <p className=" font-light mt-4 mb-8 w-[205px]">
