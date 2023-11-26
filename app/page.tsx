b3ccba1235cc7deeac1bd5715aa3de8fbdb0a4eb
Harr-Yates <hyates1@gmail.com>
2023-11-26 14:01:36 +0100

    Add metadata and head tag to page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { getProjects as fetchProjects } from "@/app/sanity/sanity-utils";
 import Links from "./components/Links";
 import VideoCard from "./components/VideoCard";
@@ -5,11 +6,33 @@ import { Metadata } from "next";
 
 // "https://cdn.sanity.io/files/ofj891ge/production/7acccd5325763e1df6cc39fde0cef6fa6f365232.png"
 
-export const metadata: Metadata = {
+type OGImage = {
+  url: string;
+  width: number;
+  height: number;
+  alt: string;
+};
+
+type MetadataType = {
+  title: string;
+  description: string;
+  openGraph?: {
+    images: OGImage[];
+  };
+};
+
+export const metadata: MetadataType = {
   title: "Harry Yates | Fullstack Developer",
   description: "Portfolio",
   openGraph: {
-    images: "/share.png",
+    images: [
+      {
+        url: "/share.png",
+        width: 1200,
+        height: 630,
+        alt: "Harry Yates",
+      },
+    ],
   },
 };
 
@@ -18,6 +41,20 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col min-h-screen mx-auto max-w-5xl px-8 md:px-16 lg:px-8">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta
+          name="description"
+          content={metadata.description}
+        />
+        {metadata.openGraph?.images.map((image, index) => (
+          <meta
+            key={index}
+            property="og:image"
+            content={image.url}
+          />
+        ))}
+      </Head>
       <header className="w-full mx-auto flex flex-col justify-between items-start">
         <h1 className=" font-medium inline mt-20">Harry Yates</h1>
         <p className=" font-light mt-4 mb-8 w-[205px]">
