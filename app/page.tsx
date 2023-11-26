dabd8f2b3aed649398866cff547c36cf8b6f4c0c
Harr-Yates <hyates1@gmail.com>
2023-11-26 15:15:54 +0100

    Add meta tag for og:image

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,13 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col min-h-screen mx-auto max-w-5xl px-8 md:px-16 lg:px-8">
+      <head>
+        <title>Hello world</title>
+        <meta
+          property="og:image"
+          content="https://portfolio-v3-18qkugzpr-harry-yates.vercel.app/opengraph-image.png?3d69783f09f78aa1"
+        />
+      </head>
       <header className="w-full mx-auto flex flex-col justify-between items-start">
         <h1 className=" font-medium inline mt-20">Harry Yates</h1>
         <p className=" font-light mt-4 mb-8 w-[205px]">
