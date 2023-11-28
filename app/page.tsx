7f86238faf9d120489ee897853dde9115ecb35fb
Harr-Yates <hyates1@gmail.com>
2023-11-28 11:38:21 +0100

    Update page layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,9 @@ export default async function Home() {
   const projects = await fetchProjects();
 
   return (
-    <div className="flex flex-col min-h-screen mx-auto max-w-5xl px-8 md:px-16 lg:px-8 lg:mt-8">
+    <div
+      className="flex flex-col min-h-screen mx-auto max-w-5xl px-8 md:px-16 lg:px-8 lg:pt-11
+    ">
       <header className="w-full mx-auto flex flex-col justify-between items-start">
         <h1 className=" font-medium inline mt-20">Harry Yates</h1>
         <p className=" font-light mt-4 mb-8 w-[205px]">
